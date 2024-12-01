15a1223de5016aaa329ab80d80099b4a8377e675
Alexander Yordanov <105381567+FireWolf240Playz@users.noreply.github.com>
2024-12-01 15:49:36 +0200

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom", // Use jsdom for DOM-based tests
+  testEnvironment: "jest-environment-jsdom",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
   testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Optional: Additional setup
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
